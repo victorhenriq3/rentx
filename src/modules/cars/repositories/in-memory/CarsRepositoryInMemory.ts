2461ce37c540ed7803338a6bf27f9fa22bd099b3
victorhenriqc <vic.1601@outlook.com>
2022-04-30 09:14:13 -0300

    create a createcarsinmemory

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -1,10 +1,31 @@
 import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDT0";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 
 import { ICarsRepository } from "../ICarsRepository";
 
 class CarsRepositoryInMemory implements ICarsRepository {
-  create(data: ICreateCarDTO): Promise<void> {
-    throw new Error("Method not implemented.");
+  cars: Car[] = [];
+  async create({
+    brand,
+    category_id,
+    daily_rate,
+    description,
+    fine_amount,
+    name,
+    license_plate,
+  }: ICreateCarDTO): Promise<void> {
+    const cars = new Car();
+    Object.assign(cars, {
+      brand,
+      category_id,
+      daily_rate,
+      description,
+      fine_amount,
+      name,
+      license_plate,
+    });
+
+    this.cars.push(cars);
   }
 }
 
