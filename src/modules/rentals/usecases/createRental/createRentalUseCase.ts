fe6333be7bffcae91b65bcef358be6c6b5d8bb88
victorhenriqc <vic.1601@outlook.com>
2022-05-12 15:30:15 -0300

    create a create rental repository in memory

---
+++ src/modules/rentals/usecases/createRental/createRentalUseCase.ts
@@ -0,0 +1,38 @@
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+
+import { AppError } from "@shared/errors/AppError";
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+class CreateRentalUseCase {
+  constructor(private rentalsRepository: IRentalsRepository) {}
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: IRequest): Promise<void> {
+    // Nao deve ser possivel cadastrar um novo aluguel caso ja exista um aberto para o mesmo carro
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      car_id
+    );
+
+    if (carUnavailable) {
+      throw new AppError("Car is unavailable");
+    }
+
+    // nao deve ser possivel cadastrar um novo aluguel caso ja exista um aberto para o memso usuario
+    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
+      user_id
+    );
+
+    if (rentalOpenToUser) {
+      throw new AppError("There is a rental in progress for user");
+    }
+  }
+}
+
+export { CreateRentalUseCase };
