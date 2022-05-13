526d9a1aeaf9347d456f4a579b3a5f62c37a8c05
victorhenriqc <vic.1601@outlook.com>
2022-05-12 21:49:48 -0300

    should not to be able to create a new rental if there is another open to the same user

--- src/modules/rentals/usecases/createRental/createRentalUseCase.spec.ts
+++ src/modules/rentals/usecases/createRental/createRentalUseCase.spec.ts
@@ -1,5 +1,7 @@
 import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
 
+import { AppError } from "@shared/errors/AppError";
+
 import { CreateRentalUseCase } from "./createRentalUseCase";
 
 let createRentalUseCase: CreateRentalUseCase;
@@ -20,5 +22,21 @@ describe("Create rental", () => {
 
     expect(rental).toHaveProperty("id");
     expect(rental).toHaveProperty("start_date");
+  });
+
+  it("should not to be able to create a new rental if there is another open to the same user", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: "12345",
+        car_id: "121212",
+        expected_return_date: new Date(),
+      });
+
+      const rental = await createRentalUseCase.execute({
+        user_id: "12345",
+        car_id: "121212",
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
