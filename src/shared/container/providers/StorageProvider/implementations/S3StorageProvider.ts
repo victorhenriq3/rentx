bca4758799c169751eba42d0839487cc309cfd38
victorhenriqc <vic.1601@outlook.com>
2022-06-04 16:47:51 -0300

    add a mime lib

--- src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -1,10 +1,30 @@
-import { s3 } from "aws-sdk";
+import upload from "@config/upload";
+import { S3 } from "aws-sdk";
+import fs from "fs";
+import { resolve } from "path";
 
 import { IStorageProvider } from "../IStorageProvider";
 
 class S3StorageProvider implements IStorageProvider {
+  private client: S3;
+
+  constructor() {
+    this.client = new S3({
+      region: "sa-east-1",
+    });
+  }
   async save(file: string, folder: string): Promise<string> {
-    throw new Error("Method not implemented.");
+    const originalName = resolve(upload.tmpFolder, file);
+
+    const fileContent = await fs.promises.readFile(originalName);
+
+    await this.client.putObject({
+      Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+      Key: file,
+      ACL: "public-read",
+      Body: fileContent,
+      ContentType: 
+    });
   }
   async delete(file: string, folder: string): Promise<void> {
     throw new Error("Method not implemented.");
