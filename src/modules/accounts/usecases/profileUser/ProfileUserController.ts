5b1790cbf8861fb4bc88081ce8ff7acb908ec9fd
victorhenriqc <vic.1601@outlook.com>
2022-06-05 11:20:42 -0300

    craate a user profile controller

--- src/modules/accounts/usecases/profileUser/ProfileUserController.ts
+++ src/modules/accounts/usecases/profileUser/ProfileUserController.ts
@@ -5,7 +5,13 @@ import { ProfileUserUseCase } from "./ProfileUserUseCase";
 
 class ProfileUserContrller {
   async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.user;
+
     const profileUserUseCase = container.resolve(ProfileUserUseCase);
+
+    const user = await profileUserUseCase.execute(id);
+
+    return response.json(user);
   }
 }
 
